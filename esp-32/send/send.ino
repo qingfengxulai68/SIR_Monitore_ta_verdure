9796527f6a1a9954e93fc2f9e9a5b34eebf5a14f
giahg294 <giahoang294@gmail.com>
2026-01-17 20:05:04 +0100

    update sen.ino pour esp

--- esp-32/send/send.ino
+++ esp-32/send/send.ino
@@ -1,11 +1,5 @@
 #include <esp_now.h>
 #include <WiFi.h>
-<<<<<<< Updated upstream
-#include <esp_wifi.h> // Ajouté pour le contrôle du canal
-
-// 1. MODIFIEZ CET ID POUR CHAQUE CARTE (Plante 1, Plante 2, etc.)
-#define SENSOR_ID 2 
-=======
 #include <BH1750.h>
 #include <DHT.h>
 // Sensor Activation
@@ -32,51 +26,32 @@ BH1750 lightMeter; // luminosité
 
 
 DHT dht(DHT_PIN, DHTTYPE); // 
->>>>>>> Stashed changes
 
 uint8_t broadcastAddress[] = {0x80, 0xF3, 0xDA, 0x60, 0x40, 0xB8};
 
 typedef struct struct_message {
-<<<<<<< Updated upstream
-  int id;          
-  float temp;      
-  int hum;      
-  int moist;
-  int light;   
-  bool lowBattery; 
-=======
   int id;          // ID de la plante
   int temp;      // Température
   int hum;         // Humidité
   int moisturePercent;//Pourcentage d'humidité
   int lux;        // Luminosité
   bool lowBattery; // Alerte batterie
->>>>>>> Stashed changes
 } struct_message;
 
 struct_message myData;
 esp_now_peer_info_t peerInfo;
 
-<<<<<<< Updated upstream
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nStatut envoi : ");
-=======
 // Fonction de rappel (callback) appelée lors de l'envoi
 void OnDataSent(const wifi_tx_info_t *info, esp_now_send_status_t status) {
   Serial.print("\r\nStatut du dernier paquet : ");
->>>>>>> Stashed changes
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Succès (ACK reçu)" : "Échec");
 }
 
 void setup() {
   Serial.begin(115200);
-<<<<<<< Updated upstream
-
-=======
   delay(200);
   t0 = millis();
   // Mettre le Wi-Fi en mode Station
->>>>>>> Stashed changes
   WiFi.mode(WIFI_STA);
   // Soil moisture sensor
   // Soil moisture sensor
@@ -87,13 +62,6 @@ void setup() {
   if (lightMeterIsActivated) {
     Wire.begin(21, 22);   // SDA=21, SCL=22
 
-<<<<<<< Updated upstream
-  // 2. FORCE LE CANAL WI-FI (doit être le même que le récepteur, souvent canal 1)
-  esp_wifi_set_promiscuous(true);
-  esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
-  esp_wifi_set_promiscuous(false);
-
-=======
     if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
       Serial.println("BH1750 init OK");
     } else {
@@ -107,7 +75,6 @@ void setup() {
     dht.begin();
   }
   // Initialiser ESP-NOW
->>>>>>> Stashed changes
   if (esp_now_init() != ESP_OK) {
     Serial.println("Erreur d'initialisation ESP-NOW");
     return;
@@ -126,14 +93,6 @@ void setup() {
 }
 
 void loop() {
-<<<<<<< Updated upstream
-  // 3. UTILISE L'ID UNIQUE DÉFINI EN HAUT
-  myData.id = SENSOR_ID; 
-  myData.temp = 24.5;
-  myData.hum = analogRead(34);
-  myData.moist = 24.5;
-  myData.light = 24.5;
-=======
   if (millis() - t0 >= 5000) {
     // Code inside is executed every period
     t0 = millis();
@@ -164,17 +123,11 @@ void loop() {
   myData.hum = hum;
   myData.moisturePercent=moisturePercent;
   myData.lux= (int)lux;
->>>>>>> Stashed changes
   myData.lowBattery = false;
 
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
    
   if (result == ESP_OK) {
-<<<<<<< Updated upstream
-    Serial.print("Trame de la plante ");
-    Serial.print(SENSOR_ID);
-    Serial.println(" envoyée.");
-=======
     Serial.println("Trame envoyée avec succès");
     Serial.print("ID: "); 
     Serial.println(myData.id);
@@ -188,7 +141,6 @@ void loop() {
     Serial.println(myData.lux);
   } else {
     Serial.println("Erreur d'envoi");
->>>>>>> Stashed changes
   }
   }
 
